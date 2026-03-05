035adad279bd4804a1554a46212db7fdc12c4ee5
fabyodanapermady089-hue <fabyodanapermady089@gmail.com>
2026-03-06 00:23:58 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        NEXT_PUBLIC_GEMINI_API_KEY: process.env.NEXT_PUBLIC_GEMINI_API_KEY,
-    },
+  // Mengaktifkan fitur React Strict Mode untuk coding yang lebih bersih
+  reactStrictMode: true,
+
+  // Izinkan Next.js menampilkan gambar dari domain eksternal
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.google.com', // Mengizinkan gambar dari server Google
+      },
+      {
+        protocol: 'data', // Mengizinkan gambar dalam format Base64 (inlineData)
+        hostname: '*',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
